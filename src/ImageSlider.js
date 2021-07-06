1b92860cdbfdf7b580b35e8ca0cacb449977f9b1
dbrandt1990 <dvbrandt90@gmail.com>
2021-07-06 12:52:43 -0600

    commit tree 03507b130a61befb4234ee2c705c415570991421

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,12 @@
+import React, { Component } from 'react';
 // your ImageSlider code here!
+class ImageSlider extends React.Component {
+    state = {
+        currentSlideIndex: 0
+    }
+    render() {
+        return <div>I am on slide {this.state.currentSlideIndex}</div>
+    }
+}
+
+export default ImageSlider
